4d0a8ba71e535898ddea80e7454846da4d85b52d
zhangming <zhangming1@aixuexi.com>
2020-09-27 18:13:40 +0800

    项目初始化

---
+++ fenum/src/main/java/com/fastenum/fenum/FenumApplication.java
@@ -0,0 +1,13 @@
+package com.fastenum.fenum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( FenumApplication.class, args );
+    }
+
+}
---
+++ fenum/src/test/java/com/fastenum/fenum/FenumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastenum.fenum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
